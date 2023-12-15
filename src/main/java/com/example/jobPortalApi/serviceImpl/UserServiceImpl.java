b40e442a4c2b4ca81dbbbdf096b4ed8a3280f4dc
Dhara18 <dhareppachavaj18@gmail.com>
2023-12-15 13:57:04 +0530

    userRequestDTOToUser method created

--- src/main/java/com/example/jobPortalApi/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/example/jobPortalApi/serviceImpl/UserServiceImpl.java
@@ -1,14 +1,26 @@
 package com.example.jobPortalApi.serviceImpl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.jobPortalApi.repository.UserRepo;
+import com.example.jobPortalApi.entity.User;
+import com.example.jobPortalApi.requestDTO.UserRequestDTO;
 import com.example.jobPortalApi.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService
 {
-	@Autowired
-	UserRepo userRepo;
+	
+	public User userRequestDTOToUser(UserRequestDTO userRequestDTO)
+	{
+		User user= new User();
+		
+		user.setUsername(userRequestDTO.getUsername());
+		user.setEmail(userRequestDTO.getEmail());
+		user.setPassword(userRequestDTO.getPassword());
+		user.setUserRole(userRequestDTO.getUserRole());
+		
+		return user;
+	}
+	
+	
 }
