15e9c6d9256e64e19b25f6906c8800fd88d16263
Dhara18 <dhareppachavaj18@gmail.com>
2023-12-16 09:25:12 +0530

    company to company response dto method added

--- src/main/java/com/example/jobPortalApi/serviceImpl/CompanyServiceImpl.java
+++ src/main/java/com/example/jobPortalApi/serviceImpl/CompanyServiceImpl.java
@@ -1,12 +1,15 @@
 package com.example.jobPortalApi.serviceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.jobPortalApi.entity.Company;
 import com.example.jobPortalApi.repository.CompanyRepo;
 import com.example.jobPortalApi.requestDTO.CompanyRequestDTO;
+import com.example.jobPortalApi.responseDTO.CompanyResponseDTO;
 import com.example.jobPortalApi.service.CompanyService;
+import com.example.jobPortalApi.utility.ResponseStructure;
 
 @Service
 public class CompanyServiceImpl implements CompanyService
@@ -25,6 +28,19 @@ public class CompanyServiceImpl implements CompanyService
 		company.setWebsite(companyRequestDTO.getWebsite());
 		
 		return company;
+	}
+	
+	public CompanyResponseDTO CompanyToCompanyResponseDTO(Company company)
+	{
+		CompanyResponseDTO companyResponseDTO= new CompanyResponseDTO();
+		companyResponseDTO.setCompanyId(company.getCompanyId());
+		companyResponseDTO.setCompanyName(company.getCompanyName());
+		companyResponseDTO.setFoundedDate(company.getFoundedDate());
+		companyResponseDTO.setBusinessType(company.getBusinessType());
+		companyResponseDTO.setContactEmail(company.getContactEmail());
+		companyResponseDTO.setContactPhNum(company.getContactPhNum());
+		companyResponseDTO.setWebsite(company.getWebsite());
+		return companyResponseDTO;
 	}
 
 	
